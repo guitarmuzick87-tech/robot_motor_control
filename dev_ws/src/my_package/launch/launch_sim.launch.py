bf86d6106ec19a67c8849c973adbeeae11b346f8
Matt Anderson <guitarmuzick87@gmail.com>
2026-09-25 17:49:23 -0400

    Fixed issues with ros2 control

--- dev_ws/src/my_package/launch/launch_sim.launch.py
+++ dev_ws/src/my_package/launch/launch_sim.launch.py
@@ -2,70 +2,79 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 
-
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument, RegisterEventHandler
+from launch.event_handlers import OnProcessExit
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration
-
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution, PythonExpression
 from launch_ros.actions import Node
-
 
 
 def generate_launch_description():
 
+    package_name = 'my_package' #<--- MAKE SURE THIS MATCHES YOUR PACKAGE NAME
 
-    # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
-    # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
-
-    package_name='my_package' #<--- CHANGE ME
-
+    # Robot State Publisher
     rsp = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory(package_name),'launch','rsp.launch.py'
-                )]), launch_arguments={'use_sim_time': 'true'}.items()
+        PythonLaunchDescriptionSource([
+            os.path.join(get_package_share_directory(package_name), 'launch', 'rsp.launch.py')
+        ]),
+        launch_arguments={'use_sim_time': 'true'}.items()
     )
-    
-    world = LaunchConfiguration('world')
 
+    world = LaunchConfiguration('world')
     world_arg = DeclareLaunchArgument(
         'world',
-        default_value="empty.sdf",
+        default_value='empty.sdf',
         description='World to load'
-        )
+    )
 
-    # Include the Gazebo launch file, provided by the ros_gz_sim package
+    # Gazebo Server (Headless, Unpaused)
     gazebo_server = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py')]),
-                    launch_arguments={'gz_args': ['-s ', world], 'on_exit_shutdown': 'true'}.items()
-             )
+        PythonLaunchDescriptionSource([
+            os.path.join(get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py')
+        ]),
+        launch_arguments={
+            'gz_args': PythonExpression(["'-s -r ' + '", world, "'"]),
+            'on_exit_shutdown': 'true'
+        }.items()
+    )
 
-    # Include the Gazebo launch file, provided by the ros_gz_sim package
+    # Gazebo GUI (Frontend)
     gazebo_gui = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py')]),
-                    launch_arguments={'gz_args': ['-r ', '-v4 ', '-g '], 'on_exit_shutdown': 'true'}.items()
-             )
+        PythonLaunchDescriptionSource([
+            os.path.join(get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py')
+        ]),
+        launch_arguments={
+            'gz_args': '-g -v4',
+            'on_exit_shutdown': 'true'
+        }.items()
+    )
 
-    # Run the spawner node from the ros_gz_sim package. The entity name doesn't really matter if you only have a single robot.
-    spawn_entity = Node(package='ros_gz_sim', executable='create',
-                        arguments=['-topic', 'robot_description',
-                                   '-name', 'my_bot',
-                                   '-z', '0.1'],
-                        output='screen')
+    # Spawner Node
+    spawn_entity = Node(
+        package='ros_gz_sim',
+        executable='create',
+        arguments=[
+            '-topic', 'robot_description',
+            '-name', 'my_bot',
+            '-z', '0.1'
+        ],
+        output='screen'
+    )
 
-    # Launch the ROS-Gazebo bridge for normal topics
-    bridge_params = os.path.join(get_package_share_directory(package_name),'config','gz_bridge.yaml')
+    # ROS-Gazebo Parameter Bridge (Explicitly bridging /clock for ros2_control sim time)
+    bridge_params = os.path.join(get_package_share_directory(package_name), 'config', 'gz_bridge.yaml')
     ros_gz_bridge = Node(
         package="ros_gz_bridge",
         executable="parameter_bridge",
         arguments=[
+            '/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock',
             '/cmd_vel@geometry_msgs/msg/Twist@gz.msgs.Twist',
             '--ros-args',
-            '-p',
-            f'config_file:={bridge_params}',
-        ]
+            '-p', f'config_file:={bridge_params}',
+        ],
+        output='screen'
     )
 
     ros_gz_image_bridge = Node(
@@ -74,6 +83,7 @@ def generate_launch_description():
         arguments=["/camera/image_raw"]
     )
 
+    # Controller Spawners
     diff_drive_spawner = Node(
         package="controller_manager",
         executable="spawner",
@@ -90,7 +100,21 @@ def generate_launch_description():
         arguments=["joint_broad"],
     )
 
-    # Launch them all!
+    # Sequence Spawners AFTER robot entity is spawned in Gazebo
+    delayed_diff_drive_spawner = RegisterEventHandler(
+        event_handler=OnProcessExit(
+            target_action=spawn_entity,
+            on_exit=[diff_drive_spawner],
+        )
+    )
+
+    delayed_joint_broad_spawner = RegisterEventHandler(
+        event_handler=OnProcessExit(
+            target_action=spawn_entity,
+            on_exit=[joint_broad_spawner],
+        )
+    )
+
     return LaunchDescription([
         rsp,
         world_arg,
@@ -99,6 +123,6 @@ def generate_launch_description():
         spawn_entity,
         ros_gz_bridge,
         ros_gz_image_bridge,
-        diff_drive_spawner,
-        joint_broad_spawner,
+        delayed_diff_drive_spawner,
+        delayed_joint_broad_spawner,
     ])
